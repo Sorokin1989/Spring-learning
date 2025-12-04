5cd17cc31175c96486ebe92cdceda0c28d4fbaf6
Dmitrii <sorokindm1try@yandex.ru>
2025-12-04 17:29:22 +0300

    homework7 1.2

--- src/main/java/ru/top/homework7/controller/HomeController.java
+++ src/main/java/ru/top/homework7/controller/HomeController.java
@@ -3,7 +3,9 @@ package ru.top.homework7.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.top.homework7.Person;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -60,14 +62,47 @@ public class HomeController {
     }
     //Задание 6
     //Создайте endpoint /api/home/count, который возвращает целое число 100
+    @GetMapping("/count")
+    public int count(){
+        return 100;
+    }
     //Задание 7
     //Создайте endpoint /api/home/students, который возвращает список (List) из 3 объектов Person (студенты с разными именами и возрастами)
+    List<Person> people = List.of(
+            new Person("Дима",30),
+            new Person("Коля",25),
+            new Person("Ваня",28)
+
+    );
+    @GetMapping("/students")
+    public List<Person> getStudent(){
+        return people;
+    }
     //Задание 8
     //Создайте endpoint /api/home/cities, который возвращает список (List) из 5 названий городов
+
+    List<String>cities=List.of("Moscow","London","New York","Tokio","San Francisco");
+
+    @GetMapping("/cities")
+    public List<String>getCities(){
+        return cities;
+    }
     //Задание 9
     //Создайте endpoint /api/home/price, который возвращает дробное число (double) - например, 99.99
+
+    @GetMapping("/price")
+    public double price(){
+        return 99.99;
+    }
     //Задание 10
     //Создайте endpoint /api/home/weekdays, который возвращает массив строк с названиями дней недели (Monday, Tuesday, и т.д.)
+
+    String[] days={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+
+    @GetMapping("/weekdays")
+    public String[] getDays(){
+        return days;
+    }
 
 
 }
